3a0934a53ee1dc5ed735a81e078ac08457f707de
martin byrne <marb61a@gmail.com>
2016-05-12 13:08:50 +0000

    updated  webpack server file

--- webpack.server.js
+++ webpack.server.js
@@ -9,7 +9,35 @@ function ignoreNodeModules(context, request, callback){
 }
 
 function createConfig(isDebug){
+    const plugins = [];
     
+    if (!isDebug) {
+		plugins.push(new webpack.optimize.UglifyJsPlugin());
+	}
+	
+	// Webpack Config
+	return{
+		target : "node",
+		devtool : "source-map",
+		entry: "./src/server/server.js",
+		output: {
+			path: path.join(__dirname, "build"),
+			filename: "server.js"
+		},
+		resolve: {
+			alias: {
+				shared: path.join(__dirname, "src", "shared")
+			}
+		},
+		module: {
+			loaders: [
+				{ test: /\.js$/, loader: "babel", exclude: /node_modules/ },
+				{ test: /\.js$/, loader: "eslint-loader", exclude: /node_modules/ }
+			]
+		},
+		externals: [ignoreNodeModules],
+		plugins: plugins
+	};
 }
 
 module.exports = createConfig(true);
