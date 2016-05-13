6a2cdfe33664982d1bc4c18cd6a127b1615c34b0
martin byrne <marb61a@gmail.com>
2016-05-13 10:16:46 +0000

    added webpack config to client file

--- webpack.client.js
+++ webpack.client.js
@@ -14,6 +14,35 @@ const dirname = path.resolve("./");
 
 function createConfig(isDebug){
     
+    // Webpack Config
+    return{
+    	devtool : devTool,
+    	entry: {
+			application: appEntry,
+			vendor: vendorModules
+		},
+		output: {
+			path: path.join(dirname, "public", "build"),
+			filename: "[name].js",
+			publicPath: "/build/"
+		},
+		resolve: {
+			alias: {
+				shared: path.join(dirname, "src", "shared")
+			}
+		},
+		module: {
+			loaders: [
+				{ test: /\.js$/, loader: "babel", exclude: /node_modules/ },
+				{ test: /\.js$/, loader: "eslint", exclude: /node_modules/ },
+				{ test: /\.(png|jpg|jpeg|gif|woff|ttf|eot|svg|woff2)/, loader: "url-loader?limit=1024" },
+				cssLoader,
+				sassLoader
+			]
+		},
+		plugins: plugins
+    };
+    
 }
 
 module.exports = createConfig(true);
