149d0da793c15ab7851830465505fbdf96648ffa
martin byrne <marb61a@gmail.com>
2016-05-16 10:53:10 +0000

    added initial constructor and try catch to user component

--- src/client/components/users/users.js
+++ src/client/components/users/users.js
@@ -3,5 +3,23 @@ import {ElementComponent} from "../../lib/component";
 import "./users.scss";
 
 class UsersComponent extends ElementComponent {
+    constructor(usersStore){
+        super("ul");
+        this.$element.addClass("users");
+		this._users = usersStore;
+    }
+    
+    _onAttach(){
+        
+    }
+}
+
+let component;
+
+try{
+    
+}catch(e){
+    
+}finally{
     
 }
