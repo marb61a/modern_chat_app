ef746642782a9d63412445bae8c1a4aabb347e85
martin byrne <marb61a@gmail.com>
2016-06-07 10:29:27 +0000

    updated app js

--- src/client/application.js
+++ src/client/application.js
@@ -1,1 +1,29 @@
+import $ from "jquery";
+import "moment-duration-format";
+
+import "shared/operators";
+
+import "./application.scss";
+
+import * as services from "./services";
+
+// Playground
+services.server
+        .emitActions("login", {username: "foo", password: "bar"})
+        .subscribe(user => {
+    		console.log("We're logged in: " + user);
+    	}, error => {
+    		console.error(error);
+    	});
+
+// Auth
+
+// Components
+require("./components/player/player");
+require("./components/users/users");
+require("./components/chat/chat");
+require("./components/playlist/playlist");
+
+// Bootstrap
+services.socket.connect();
 
