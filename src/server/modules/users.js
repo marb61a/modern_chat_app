51c93186fc152e51ea79bf64fbd45c2e96f88ce7
martin byrne <marb61a@gmail.com>
2016-06-10 10:32:35 +0000

    updated users module

--- src/server/modules/users.js
+++ src/server/modules/users.js
@@ -1,4 +1,12 @@
+import _ from "lodash";
+import {Observable} from "rxjs";
 import {ModuleBase} from "../lib/module";
+
+import {ModuleBase} from "../lib/module";
+import {validateLogin} from "shared/validation/users";
+import {fail} from "shared/observable-socket";
+
+const AuthContext = Symbol("AuthContext");
 
 export class UsersModule extends ModuleBase{
     constructor(){
@@ -8,9 +16,27 @@ export class UsersModule extends ModuleBase{
 		this._users = {};
     }
     
+    loginClient$(client, username){
+        username = username.trim();
+    }
+    
+    logoutClient(client){
+        
+    }
+    
     registerClient(client){
         client.onActions({
-            
+            "users:list": () => {
+				return this._userList;
+			},
+			
+			"auth:login": ({name}) => {
+				return this.loginClient$(client, name);
+			},
+			
+			"auth:logout": () => {
+				this.logoutClient(client);
+			}
         });
         
         client.on("disconnect", () => {
