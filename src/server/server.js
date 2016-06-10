41ba4f74a989e445d9aef8a4f8a2574b67d18a0c
martin byrne <marb61a@gmail.com>
2016-06-10 10:15:35 +0000

    updated server.js with webpack client function

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,16 @@
+import "source-map-support/register";
+
 import express from "express";
 import http from "http";
 import socketIo from "socket.io";
 import chalk from "chalk";
+import {Observable} from "rxjs";
+
+import "shared/operators";
+
+import {UsersModule} from "./modules/users";
+import {PlaylistModule} from "./modules/playlist";
+import {ChatModule} from "./modules/chat";
 
 const isDevelopment = process.env.NODE_ENV !== "production";
 
@@ -9,6 +18,33 @@ const isDevelopment = process.env.NODE_ENV !== "production";
 const app = express();
 const server = new http.Server(app);
 const io = socketIo(server);
+
+
+// Client Webpack
+if(process.env.USE_WEBPACK === "true"){
+    var webpackMiddleware = require("webpack-dev-middleware"),
+        webpackHotMiddleware = require("webpack-hot-middleware"),
+        webpack = require("webpack"),
+		clientConfig = require("../../webpack.client");
+		
+	const compiler = webpack(clientConfig);
+	app.use(webpackMiddleware, (compiler, {
+	    publicPath: "/build/",
+		stats: {
+			colors: true,
+			chunks: false,
+			assets: false,
+			timings: false,
+			modules: false,
+			hash: false,
+			version: false
+		}
+	}));
+	
+	app.use(webpackHotMiddleware(compiler));
+	console.log(chalk.bgRed("Using WebPack Dev Middleware! THIS IS FOR DEV ONLY!"));
+}
+
 
 // Configure Express
 app.set("view engine", "jade");
@@ -20,6 +56,7 @@ app.get("/", (req, res) => {
        useExternalStyles 
     });
 });
+
 
 // Startup
 const port = process.env.PORT || 3000;
