0443e6579387392272bb21f850e7e8c3e79a93f0
martin byrne <marb61a@gmail.com>
2016-06-12 15:48:17 +0000

    updated users store functions and constructor

--- src/client/stores/users.js
+++ src/client/stores/users.js
@@ -16,28 +16,46 @@ export class UsersStore {
         
         // Users List
         const defaultStore = {users: []};
+        const events$ = Observable.merge(
+            this._server.on$("users:list").map(opList),
+			this._server.on$("users:added").map(opAdd),
+			this._server.on$("users:removed").map(opRemove)
+        );
+        
+    }
+        
+    login$(name){
+        const validator = validateLogin(name);
+        if(validator.hasErrors)
+            return Observable.throw({message: validator.message});
+		
+		return this._server.emitAction$("auth:login", {name});
     }
     
-    function opList(users){
-        return state => {
-            state.users = users;
-            state.users.sort((l, r) => l.name.localeCompare(r.name));
-            return{
-                type: "list",
-			    state: state   
-            };
-        }
+    logout$(){
+        return this._server.emitAction$("auth:logout");
     }
+}
     
-    function opAdd(user){
-        return state => {
-            
-        }
-    }
-    
-    function opRemove(user){
-        return state => {
-            
-        }
-    }
+function opList(users){
+    return state => {
+        state.users = users;
+        state.users.sort((l, r) => l.name.localeCompare(r.name));
+        return{
+            type: "list",
+		    state: state   
+        };
+    };
+}
+
+function opAdd(user){
+    return state => {
+        
+    };
+}
+
+function opRemove(user){
+    return state => {
+        
+    };
 }
