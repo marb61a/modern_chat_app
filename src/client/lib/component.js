caf8a0bab722df5251e578025fffa4fb6e7ce9d8
martin byrne <marb61a@gmail.com>
2016-06-12 16:03:45 +0000

    updated elementcomponent

--- src/client/lib/component.js
+++ src/client/lib/component.js
@@ -1,6 +1,13 @@
 import $ from "jquery";
 
 export class ComponentBase{
+    attach($mount){
+        this._$mount = $mount;
+		this._onDetachHandlers = [];
+		this.children = [];
+		this._onAttach();
+    }
+    
     _onAttach() {
         
 	}
@@ -17,6 +24,24 @@ export class ElementComponent extends ComponentBase{
     
     constructor(elementType="div"){
         super();
-        
+        this._$element = $(`<${elementType}>`).data("component", this);
+    }
+    
+    attach($mount){
+        super.attach($mount);
+        this.$element.appendTo(this._$mount);
+    }
+    
+    detach(){
+        super.detach();
+		this.$element.remove();
+    }
+    
+    _setClass(className, isOn){
+        if(isOn){
+            this._$element.addClass(className);
+        } else {
+            this._$element.removeClass(className);
+        }
     }
 }
