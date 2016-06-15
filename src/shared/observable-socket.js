ac57415b0f04ed4818ad7bece1a4eafe2aa70134
martin byrne <marb61a@gmail.com>
2016-06-15 09:46:33 +0000

    updated observable socket

--- src/shared/observable-socket.js
+++ src/shared/observable-socket.js
@@ -20,12 +20,12 @@ export class ObservableSocket {
     	return this._state.isConnected;
     }
     
-    get isConnecting(){
-    	return this._state.isConnecting;
+    get isReconnecting(){
+    	return this._state.isReconnecting;
     }
     
     get isTotallyDead(){
-    	return !this.isConnected && !this.isConnecting;
+    	return !this.isConnected && !this.isReconnecting;
     }
     
     constructor(socket){
@@ -35,5 +35,32 @@ export class ObservableSocket {
 		this._requests = {};
 		this._nextRequestId = 0;
 		
+		this.status$ = Observable.merge(
+		    this.on$("connect").map(() => ({
+		        isConnected : true
+		    })),
+		    this.on$("disconnect").map(() => ({
+		        isConnected : false
+		    })),
+		    this.on$("reconnecting").map(attempt => ({
+		        isConnected : false, 
+		        isReconnecting : true
+		        
+		    })),
+		    this.on$("reconnecting failed").map(() => ({
+		        isConnected: false, isReconnecting: false
+		    }))
+		)
+		.publshReplay(1)
+		.refCount();
+		
+		this.status$.subscribe(state => this._state = state);
     }
+    
+    // Basic Wrappers
+    on$(event) {
+		return Observable.fromEvent(this._socket, event);
+	}
+	
+	
 }
