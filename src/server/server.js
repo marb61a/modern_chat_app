f8a752fe4a4e1be03dfd967aece9af7d7f2ce857
martin byrne <marb61a@gmail.com>
2016-06-20 10:05:01 +0000

    updated server js

--- src/server/server.js
+++ src/server/server.js
@@ -7,12 +7,17 @@ import chalk from "chalk";
 import {Observable} from "rxjs";
 
 import "shared/operators";
+import {ObservableSocket} from "shared/observable-socket";
+
+import {FileRepository} from "./repositories/file";
+import {YoutubeService} from "./services/youtube";
 
 import {UsersModule} from "./modules/users";
 import {PlaylistModule} from "./modules/playlist";
 import {ChatModule} from "./modules/chat";
 
 const isDevelopment = process.env.NODE_ENV !== "production";
+
 
 // Setup
 const app = express();
@@ -55,6 +60,30 @@ app.get("/", (req, res) => {
     res.render("index",{
        useExternalStyles 
     });
+});
+
+
+// Services
+const videoServices = [new YoutubeService("{fixme}")];
+const playlistRepository = new FileRepository("./data/playlist.json");
+
+
+// Modules
+const users = new UsersModule(io);
+const chat = new ChatModule(io, users);
+const playlist = new PlaylistModule(io, users, playlistRepository, videoServices);
+const modules = [users, chat, playlist];
+
+
+// Socket
+io.on("connection", socket => {
+	console.log(`Got connection from ${socket.request.connection.remoteAddress}`);
+	const client = new ObservableSocket(socket);
+	for (let mod of modules)
+		mod.registerClient(client);
+		
+	for (let mod of modules)
+		mod.clientRegistered(client);
 });
 
 
