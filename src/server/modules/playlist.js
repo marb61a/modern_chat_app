75220e4dbe768f68b152dded43f990ad32d3d006
martin byrne <marb61a@gmail.com>
2016-06-21 10:13:47 +0000

    updated playlist module

--- src/server/modules/playlist.js
+++ src/server/modules/playlist.js
@@ -19,9 +19,38 @@ export class PlayListModule extends ModuleBase{
 		this._currentSource = null;
 		this._currentTime = 0;
 		
+		setInterval(this._tickUpdateTime.bind(this), 1000);
+		setInterval(this.tickUpdateClients.bind(this), 5000);
     }
     
     init$(){
-        
+        return this._repository.getAll$().do(this.setPlaylist.bind(this));
+    }
+    
+    getSourceById(){
+    	return _.find(this._playlist, { id });
+    }
+    
+    setPlaylist(playlist){
+    	this._playlist = playlist;
+    	for(let source of playlist)
+    		source.id = this._nextSourceId++;
+    	
+    	this._io.emit("playlist:list", this._playlist);
+    }
+    
+    setCurrentSource(source){
+    	if(source == null){
+    		this._currentSource = null;
+    		this._currentIndex = this._currentTime = 0;
+    	} else{
+    		const newIndex = this._playlist.indexOf(source);
+    		if (newIndex === -1)
+    			throw new Error(`Cannot set current to source ${source.id} / ${source.title}, it was not found`);
+    		
+    		this._currentTime = 0;
+			this._currentSource = source;
+			this._currentIndex = newIndex;
+    	}
     }
 }
