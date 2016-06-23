c6226504bb33b0c0a375c3f7dec6d8baa8b6fed7
martin byrne <marb61a@gmail.com>
2016-06-23 10:10:58 +0000

    updated player component

--- src/client/components/player/player.js
+++ src/client/components/player/player.js
@@ -1,11 +1,14 @@
 import $ from "jquery";
 import _ from "lodash";
+import {Observable} from "rxjs";
+import {ElementComponent} from "../../lib/component";
 
 import {ElementComponent} from "../../lib/component";
 
 import "./player.scss";
 
 import {playlistStore} from "../../services";
+import {YoutubePlayer} from "./players/youtube";
 
 class PlayerComponent extends ElementComponent{
     constructor(playlistStore){
@@ -15,8 +18,24 @@ class PlayerComponent extends ElementComponent{
     }
     
     _onAttach(){
+        const $title = this._$mount.find("h1");
         
-    }
+        this._players = {
+            youtube : new YoutubePlayer()
+        };
+        
+        for(let type in this._players){
+            if(this._players.hasOwnProperty(type))
+                continue;
+            
+            this._players[type].attach(this.$element);
+        }
+        
+        const initList = _.map(this._players, player => player.init$());
+        Observable.merge(...initList)
+            .toArray()
+            .compSubscribe(this, this._playersAttached.bind(this));
+     }
     
 }
 
@@ -24,8 +43,13 @@ let component;
 
 try{
     component = new PlayerComponent(playlistStore);
+    component.attach($("section.player"));
 } catch (e) {
     console.error(e);
+    if(component)
+        component.detach();
 } finally {
-    
+    if(module.hot)
+        module.hot.accept();
+        module.hot.dispose(() => component && component.detach());
 }
