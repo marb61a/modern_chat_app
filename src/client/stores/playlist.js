b788bc74140c4d27299a1f58ae954b4fe4af2447
martin byrne <marb61a@gmail.com>
2016-06-27 10:05:38 +0000

    updated playlist store

--- src/client/stores/playlist.js
+++ src/client/stores/playlist.js
@@ -11,16 +11,68 @@ export class PlaylistStore {
         };
         
         this._server = server;
-        const events$ = Observable.merge();
+        const events$ = Observable.merge(
+            server.on$("playlist:list").map(opList),
+			server.on$("playlist:added").map(opAdd),
+			server.on$("playlist:current").map(opCurrent),
+			server.on$("playlist:removed").map(opRemove),
+			server.on$("playlist:moved").map(opMove));
         
         this.actions$ = events$
             .scan(
                 ({state}, op) => op(state),
                 {state : defaultState}
-            );
+            )
+            .publish();
+        
+        const publishState$ = this.actions$.publishReplay(1);
+        
     }
     
     addSource$(url){
         const validator = validateAddSource(url);
+        if (!validator.isValid)
+			return Observable.throw({ message: validator.message });
+
+		return this._server.emitAction$("playlist:add", { url });
     }
+}
+
+function opList(sources){
+    return state => {
+        
+    };
+}
+
+function opAdd(){
+    return state => {
+        
+    };
+}
+
+function opCurrent(){
+    return state => {
+        
+    };
+}
+
+function opRemove(){
+    return state => {
+        
+    };
+}
+
+function opMove(){
+    return state => {
+        
+    };
+}
+
+function opError(state, error){
+    console.error(error);
+    return {
+		type: "error",
+		error: error,
+		state: state
+	};
 }
