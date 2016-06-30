22249bd58299d98846481da2bd6c3a62ad14e9be
martin byrne <marb61a@gmail.com>
2016-06-30 10:05:48 +0000

    updated playlist chrome component

--- src/client/components/playlist/chrome.js
+++ src/client/components/playlist/chrome.js
@@ -12,5 +12,25 @@ export class PlaylistChromeComponent extends ComponentBase {
     
     _onAttach(){
         this._$header.html("");
+        const $title = $(`<span class="title" />`).appendTo(this._$header);
+		const $stats = $(`<span class="info" />`).appendTo(this._$header);
+		
+		const updateActions$ = this._playlist.actions$
+		    .filter(a => a.type == "add" || a.type == "remove" || a.type == "list");
+		    
+		this._playlist.state$.first()
+		    .merge(updateActions$)
+		    .compSubscribe(this, ({state}) => {
+		        const totalTime = state.list.reduce((time, source) => time + source.totalTime, 0);
+		        $title.text(`${state.list.length} item${state.list.length == 1 ? "" : "s"}`);
+		        $stats.text(moment.duration(totalTime, "seconds").format());
+		    });
+		
+		const $indicator = $(`<div class="playing-indicator" />`).appendTo(this._$mount);
+		let lastSource;
+		this._playlist.state$
+		    .compSubscribe(this, ({state, type}) => {
+		        
+		    });
     }
 }
