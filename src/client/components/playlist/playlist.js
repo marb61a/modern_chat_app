28cb84f9ea7f249124c55ef045e690af6d20c5e0
martin byrne <marb61a@gmail.com>
2016-07-06 09:36:42 +0000

    updated playlist component

--- src/client/components/playlist/playlist.js
+++ src/client/components/playlist/playlist.js
@@ -17,15 +17,45 @@ class PlaylistComponent extends ComponentBase {
 		this._users = usersStore;
 	}
 	
+	_onAttach(){
+	    const $title = this._$mount.find("> h1");
+		
+		const toolbar = new PlaylistToolbarComponent(this._playlist);
+		toolbar.attach(this._$mount);
+		
+		this._$chrome = $(`<div class="chrome" />`).appendTo(this._$mount);
+		this._$scrollArea = $(`<div class="scroll-area" />`).appendTo(this._$chrome);
+		
+		const list = new PlaylistListComponent(this._playlist, this._users);
+		list.attach(this._$scrollArea);
+		
+		const contextMenu = new PlaylistContextMenuComponent(this._playlist, this._users, list.$element);
+		contextMenu.attach(this._$scrollArea);
+		
+		const chrome = new PlaylistChromeComponent(this._playlist, $title);
+		chrome.attach(this._$chrome);
+		
+		this.children.push(toolbar, list, contextMenu, chrome);
+	}
+	
+	_onDetach(){
+	    this._$chrome.remove();
+	}
+	
 }
 
 let component;
 
 try{
    component = new PlaylistComponent(services.playlistStore, services.usersStore);
-   
+   component.attach($("section.playlist"));
 } catch(e){
     console.error(e);
+    if(component)
+        component.detach();
 } finally{
-    
+    if(module.hot){
+        module.hot.accept();
+        module.hot.dispose(() => component && component.detach());
+    }
 }
